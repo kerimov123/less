c6d8645107e733eac19612f876ce6a9cdff4e076
Habukka <my_addres@mail.ru>
2024-01-21 23:32:29 +0300

    hello hlupic

--- git_command.py
+++ git_command.py
@@ -1,10 +1,13 @@
 print('Python 3')
 
 # git init  # инициализация git
+
 # git config --global user.email my_address@mail.ru
 # git config --global user.name saeed
+
 # git add .  # индексируем все файлы
 # git add <путь до индексируемого файла>
+
 # git reset <путь до файла>  # удаление индексации файла
 # git status  # уточнение статуса
 # git commit -m "Комментарий к коммиту"

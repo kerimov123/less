1ed87f2d9b91615090dd03437efce1833c27f065
ffd <Xaibula94@mail.ru>
2024-01-26 23:53:17 +0300

    habba

---
+++ string_type.py
@@ -0,0 +1,25 @@
+title = 'Фильм \'Мстители\''
+
+print(title)
+print(title[0:5])
+print(title[0:8])
+print(title[3:9])
+print(title[0:])
+print(title[:11])
+print(title[-9])
+print(title[-11:-5])
+print(title[:])
+print(title[::3])
+print(title[::-2])
+print(len(title))# команда len возвращает длину строки
+
+stihi = '''Когда же через шумный град
+Я пробираюсь торопливо,
+То старцы детям говорят
+С улыбкою самолюбивой:'''
+
+print(stihi)
+print(len(stihi))
+
+min = ('Хайбула')
+print(len(min))
